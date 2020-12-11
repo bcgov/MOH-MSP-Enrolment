1ddc19469dde42688bf4bf5502c6ac602ac0b32d
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-12-10 17:26:59 -0800

    Debug version issues

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5816f86';
-export const buildTime = '12/10/2020 at 3:25:08 PM';
+export const gitCommit = '730db54';
+export const buildTime = '12/10/2020 at 4:56:55 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 5816f86 - 12/10/2020 at 3:25:08 PM (v3.3.1) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/5816f86';
+export const message = 'Canonical Version: 730db54 - 12/10/2020 at 4:56:55 PM (v3.3.1) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/730db54';
 /** If true, other values should be present. */
 export const success = true;
