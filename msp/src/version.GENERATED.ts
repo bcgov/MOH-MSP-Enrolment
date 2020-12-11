a77cf7de4e77d9e9d581bf5186c5695c2823d118
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-12-10 16:24:57 -0800

    adjust version generation

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '234e45df';
-export const buildTime = '10/27/2020 at 10:38:50 PM';
+export const gitCommit = '5816f86';
+export const buildTime = '12/10/2020 at 3:25:08 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 234e45df - 10/27/2020 at 10:38:50 PM (v3.3.1) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/234e45df';
+export const message = 'Canonical Version: 5816f86 - 12/10/2020 at 3:25:08 PM (v3.3.1) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/5816f86';
 /** If true, other values should be present. */
 export const success = true;
