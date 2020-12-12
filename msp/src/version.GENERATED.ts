4c937a965fdecebd566c5caf2fd757dbd29f517d
jam <jam@avoxtech.ca>
2020-12-11 17:43:17 -0800

    removing version.js and generated

--- msp/src/version.GENERATED.ts
+++
@@ -1,9 +0,0 @@
-// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'c655f39';
-export const buildTime = '2020-12-11 at 5:00:06 p.m.';
-/** App version retrieved from package.json. */
-export const projectVersion = '3.3.2';
-/** Human readable message  */
-export const message = 'Canonical Version: c655f39 - 2020-12-11 at 5:00:06 p.m. (v3.3.2) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/c655f39';
-/** If true, other values should be present. */
-export const success = true;
