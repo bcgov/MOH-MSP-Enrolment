af25d199958667f92603f27d9e5c7944cbff4c62
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-12-15 14:38:41 -0800

    Remove test .env

---
+++ msp/src/version.GENERATED.ts
@@ -0,0 +1,9 @@
+// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
+export const gitCommit = '1627294';
+export const buildTime = '12/15/2020 at 2:11:41 PM';
+/** App version retrieved from package.json. */
+export const projectVersion = '3.3.2';
+/** Human readable message  */
+export const message = 'Canonical Version: 1627294 - 12/15/2020 at 2:11:41 PM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/1627294';
+/** If true, other values should be present. */
+export const success = true;
