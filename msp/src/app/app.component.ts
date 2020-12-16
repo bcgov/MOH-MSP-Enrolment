c2a7a57a584a35a5eeb00887a0d38490ba7e0cc0
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-12-15 17:22:50 -0800

    Remove console log

--- msp/src/app/app.component.ts
+++ msp/src/app/app.component.ts
@@ -60,8 +60,6 @@ export class GeneralAppComponent {
       this.headerName = title;
     });
 
-    console.log('version is:', version);
-
     version.success
       ? console.log("%c" + version.message, "color: #036; font-size: 20px;")
       : console.error(version.message);
