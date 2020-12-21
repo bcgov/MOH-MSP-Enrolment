e8123784832a72373aeed16b26275d38ff69f30e
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-12-21 11:11:19 -0800

    Update request letter component

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '1627294';
-export const buildTime = '12/15/2020 at 2:11:41 PM';
+export const gitCommit = '96ec9a9';
+export const buildTime = '12/21/2020 at 11:07:34 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 1627294 - 12/15/2020 at 2:11:41 PM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/1627294';
+export const message = 'Canonical Version: 96ec9a9 - 12/21/2020 at 11:07:34 AM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/96ec9a9';
 /** If true, other values should be present. */
 export const success = true;
