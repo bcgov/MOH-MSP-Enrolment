7d51c7adcab33c34bc2b41fee06ea3c40530259f
Bryce McMath <bryce.j.mcmath@gmail.com>
2021-01-06 14:16:57 -0800

    Update version files

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '7e1db3c';
-export const buildTime = '1/5/2021 at 1:45:56 PM';
+export const gitCommit = '6d13698f';
+export const buildTime = '11/24/2020 at 10:47:24 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 7e1db3c - 1/5/2021 at 1:45:56 PM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/7e1db3c';
+export const message = 'Canonical Version: 6d13698f - 11/24/2020 at 10:47:24 AM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/6d13698f';
 /** If true, other values should be present. */
 export const success = true;
