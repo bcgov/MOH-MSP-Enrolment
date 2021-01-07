ee7aaea37d944927f013365f48a16eb4f48b09b6
jam <jam@avoxtech.ca>
2021-01-07 11:10:05 -0800

    testing fix to node-sass issue

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '6d13698f';
-export const buildTime = '11/24/2020 at 10:47:24 AM';
+export const gitCommit = 'ef28b1d';
+export const buildTime = '2021-01-07 at 11:09:06 a.m.';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.2';
 /** Human readable message  */
-export const message = 'Canonical Version: 6d13698f - 11/24/2020 at 10:47:24 AM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/6d13698f';
+export const message = 'Canonical Version: ef28b1d - 2021-01-07 at 11:09:06 a.m. (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/ef28b1d';
 /** If true, other values should be present. */
 export const success = true;
