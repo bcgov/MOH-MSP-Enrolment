022f9cf9b3e8bec815536e88606e5248425f89da
Bryce McMath <bryce.j.mcmath@gmail.com>
2021-01-12 13:31:59 -0800

    Revert lib updates

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'ef28b1d';
-export const buildTime = '2021-01-07 at 11:09:06 a.m.';
+export const gitCommit = 'cae9759';
+export const buildTime = '1/12/2021 at 1:24:29 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.3.2';
 /** Human readable message  */
-export const message = 'Canonical Version: ef28b1d - 2021-01-07 at 11:09:06 a.m. (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/ef28b1d';
+export const message = 'Canonical Version: cae9759 - 1/12/2021 at 1:24:29 PM (v3.3.2) \nhttps://github.com/bcgov/MyGovBC-MSP/commits/cae9759';
 /** If true, other values should be present. */
 export const success = true;
