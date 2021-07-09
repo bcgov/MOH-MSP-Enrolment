67037e60398fff5bd3844afcd3603c69f25baaad
Bryce McMath <bryce.j.mcmath@gmail.com>
2021-07-09 15:21:42 -0700

    Get dynamic titles working except for supben

--- msp/src/app/app.component.ts
+++ msp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewContainerRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser'
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { environment } from '../environments/environment';
@@ -9,23 +10,26 @@ import { SpaEnvService } from './services/spa-env.service';
 // import { } from '../version.GENERATED';
 
 @Component({
-  selector: 'general-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-
+  selector: "general-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class GeneralAppComponent {
   private viewContainerRef: ViewContainerRef;
   routerSubscription: Subscription;
   headerSubscription: Subscription;
+  titleSubscription: Subscription;
   // Even though we update this from headerService, we still want to set default value to avoid pop-in.
   public headerName: string = environment.appConstants.serviceName;
 
-
-  public constructor( viewContainerRef: ViewContainerRef,
-                      private router: Router,
-                      private header: HeaderService,
-                      private spaEnvService: SpaEnvService) {
+  public constructor(
+    viewContainerRef: ViewContainerRef,
+    private router: Router,
+    private header: HeaderService,
+    private spaEnvService: SpaEnvService,
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute
+  ) {
     // You need this small hack in order to catch application root view container ref
     this.viewContainerRef = viewContainerRef;
 
@@ -33,60 +37,80 @@ export class GeneralAppComponent {
     this.handleRefresh(location.pathname);
   }
 
+
+
   handleRefresh(url) {
     // Refresh on account change
-    if (url.includes('/deam')) {
+    if (url.includes("/deam")) {
       // if on the home page, don't redirect but remove any stored application data
-      if (url.includes('/home')) {
+      if (url.includes("/home")) {
         this.clearStorage();
         // if anywhere else besides confirmation, redirect them to the home page
-      } else if (!url.includes('/confirmation')) {
-        this.hardRedirect('/msp/deam/home');
+      } else if (!url.includes("/confirmation")) {
+        this.hardRedirect("/msp/deam/home");
       }
       // Refresh on retro assistance
-    } else if (url.includes('/assistance')) {
+    } else if (url.includes("/assistance")) {
       // if on the home page, don't redirect but remove any stored application data
-      if (url.includes('/home')) {
+      if (url.includes("/home")) {
         this.clearStorage();
         // if anywhere else besides confirmation, redirect them to the home page
-      } else if (!url.includes('/confirmation')) {
-        this.hardRedirect('/msp/assistance/home');
+      } else if (!url.includes("/confirmation")) {
+        this.hardRedirect("/msp/assistance/home");
       }
-    } else if (url.includes('/benefit')) {
+    } else if (url.includes("/benefit")) {
       // if on the home page, don't redirect but remove any stored application data
-      if (url.includes('/eligibility')) {
+      if (url.includes("/eligibility")) {
         this.clearStorage();
         // if anywhere else besides confirmation, redirect them to the home page
-      } else if (!url.includes('/confirmation')) {
-        this.hardRedirect('/msp/benefit/eligibility');
+      } else if (!url.includes("/confirmation")) {
+        this.hardRedirect("/msp/benefit/eligibility");
       }
     }
   }
 
   ngOnInit() {
     this.routerSubscription = this.router.events
-      .pipe(
-        filter(event => event instanceof NavigationEnd)
-      ).subscribe(event => {
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
         document.body.scrollTop = 0;
       });
 
-      const prefix = environment.appConstants.serviceName;
-      this.headerSubscription = this.header.title.subscribe(title => {
-        this.headerName = title;
-      });
+    const prefix = environment.appConstants.serviceName;
+    this.headerSubscription = this.header.title.subscribe(title => {
+      this.headerName = title;
+    });
 
-      version.success
-            ? console.log('%c' + version.message, 'color: #036; font-size: 20px;')
-            : console.error(version.message);
-      this.spaEnvService.loadEnvs().subscribe(response => this.spaEnvService._values.next(response));
+    version.success
+      ? console.log("%c" + version.message, "color: #036; font-size: 20px;")
+      : console.error(version.message);
+    this.spaEnvService
+      .loadEnvs()
+      .subscribe(response => this.spaEnvService._values.next(response));
+    
+      this.titleSubscription = this.router.events.pipe(  
+        filter(event => event instanceof NavigationEnd),  
+      ).subscribe(() => {  
+        const rt = this.getChild(this.activatedRoute);  
+        rt.data.subscribe(data => {  
+          this.titleService.setTitle(data.title)});  
+      });
   }
 
   ngOnDestroy() {
     // note - if we add any more subscriptions, refactor to a takeUntil()
     this.routerSubscription.unsubscribe();
     this.headerSubscription.unsubscribe();
+    this.titleSubscription.unsubscribe();
   }
+
+  getChild(activatedRoute: ActivatedRoute) {  
+    if (activatedRoute.firstChild) {  
+      return this.getChild(activatedRoute.firstChild);  
+    } else {  
+      return activatedRoute;  
+    }  
+  } 
 
   clearStorage() {
     window.sessionStorage.clear();
