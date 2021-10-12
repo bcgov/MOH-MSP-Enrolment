e1b080423e1c8be11568b74fd615cd2399eb2f7f
Harry Maynard <harry.maynard@maximusbc.ca>
2021-10-12 14:51:46 -0400

    Personal Info page fields.

--- jha/src/views/enrolment/PersonalInfoPage.vue
+++ jha/src/views/enrolment/PersonalInfoPage.vue
@@ -2,9 +2,65 @@
   <div>
     <PageContent :deltaHeight='pageContentDeltaHeight'>
       <div class="container pt-3 pt-sm-5 mb-3">
-        <h1>Form</h1>
+        <h1>Add personal information and upload documents</h1>
+        <h2>Account Holder basic information</h2>
+        <p>After enrolment, the main applicant will be the Medical Services Plan Account Holder, and will be responsible for maintaining the Medical Services Plan account and requesting any changes.</p>
         <hr class="mt-0"/>
-        <p>Form here.</p>
+        <Input label="First name"
+          id="first-name"
+          class="mb-3"
+          v-model="firstName" />
+        <Input label="Middle name (optional)"
+          id="middle-name"
+          class="mb-3"
+          v-model="middleName" />
+        <Input label="Last name"
+          id="last-name"
+          class="mb-3"
+          v-model="lastName" />
+        <DateInput label="Birthdate"
+          id="birthdate"
+          class="mb-3"
+          v-model="birthdate" />
+        <DigitInput label="Social Insurance Number (SIN)"
+          id="social-insurance-number"
+          class="mb-3"
+          v-model="socialInsuranceNumber"/>
+        <Radio label="Gender"
+          name="gender"
+          class="mb-3"
+          v-model="gender"
+          :items="genderOptions" />
+        
+        <h2>Your status in Canada</h2>
+        <p>Please provide you immigration status information. You will be required to upload documents to support your status in Canada.</p>
+        <hr/>
+        <Select label="Immigration status in Canada"
+          id="immigration-status"
+          class="mb-3"
+          v-model="immigrationStatus"
+          :options="immigrationStatusOptions"/>
+        <Radio name="citizen-status-reason"
+          class="mb-3"
+          v-model="citizenStatusReason"
+          :items="citizenStatusReasonOptions" />
+        
+        <h2>Documents</h2>
+        <p>Provide one of the following documents to support your status in Canada. If your name has changed since your ID was issued you are also required to upload document to support the name change.</p>
+        <hr/>
+        <Select label="Document Type"
+          id="citizen-support-document-type"
+          class="mb-3"
+          v-model="citizenshipSupportDocumentType"
+          :options="citizenshipSupportDocumentsOptions" />
+        <div v-if="citizenshipSupportDocumentType">
+          <h2>{{citizenshipSupportDocumentType}}</h2>
+          <hr/>
+          <FileUploader v-model="citizenshipSupportDocuments" />
+        </div>
+
+
+
       </div>
     </PageContent>
     <ContinueBar @continue="validateFields()" />
@@ -32,19 +88,56 @@ import {
 import logService from '@/services/log-service';
 import {
   ContinueBar,
+  DateInput,
+  DigitInput,
+  FileUploader,
+  Input,
   PageContent,
+  Radio,
+  Select,
 } from 'common-lib-vue';
 import pageContentMixin from '@/mixins/page-content-mixin';
+import {
+  radioOptionsGender,
+  radioOptionsCitizenStatusReasons,
+} from '@/constants/radio-options';
+import {
+  selectOptionsImmigrationStatus,
+  selectOptionsCitizenshipSupportDocuments,
+} from '@/constants/select-options';
 
 export default {
   name: 'PersonalInfoPage',
   mixins: [pageContentMixin],
   components: {
     ContinueBar,
+    DateInput,
+    DigitInput,
+    FileUploader,
+    Input,
     PageContent,
+    Radio,
+    Select,
   },
   data: () => {
-    return {};
+    return {
+      // Radio and Select options.
+      genderOptions: radioOptionsGender,
+      immigrationStatusOptions: selectOptionsImmigrationStatus,
+      citizenStatusReasonOptions: radioOptionsCitizenStatusReasons,
+      citizenshipSupportDocumentsOptions: selectOptionsCitizenshipSupportDocuments,
+      // Data to be saved.
+      firstName: null,
+      middleName: null,
+      lastName: null,
+      birthdate: null,
+      socialInsuranceNumber: null,
+      gender: null,
+      immigrationStatus: null,
+      citizenStatusReason: null,
+      citizenshipSupportDocumentType: null,
+      citizenshipSupportDocuments: [],
+    };
   },
   created() {
     logService.logNavigation(
