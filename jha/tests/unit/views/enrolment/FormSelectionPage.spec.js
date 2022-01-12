430d8be9674f5629060b141809f7259f9722cfce
Harry Maynard <harry.maynard@maximusbc.ca>
2022-01-12 15:27:53 -0500

    Fixed unit tests on form selection page.

--- jha/tests/unit/views/enrolment/FormSelectionPage.spec.js
+++ jha/tests/unit/views/enrolment/FormSelectionPage.spec.js
@@ -1,37 +1,62 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
 import FormSelectionPage from '@/views/enrolment/FormSelectionPage.vue';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
 Vue.use(Vuelidate);
 
 describe('Enrolment - FormSelectionPage.vue', () => {
   let state;
+  let router;
   let store;
 
   beforeEach(() => {
-    state ={
+    state = {
       applicationUuid: null,
     };    
 
     store = new Vuex.Store({
       modules: {
+        appModule: {
+          state: {
+            showMobileStepperDetails: false,
+          }
+        },
         enrolmentModule: {
           state,
           namespaced: true,
           actions: {
-            setApplicationUuid() {}
+            setApplicationUuid() {},
+            setIsApplyingForMSP() {},
+            setIsApplyingForFPCare() {},
+            setIsApplyingForSuppBen() {},
+          },
+          getters: {
+            isEligibleForMSP() {
+              return true;
+            },
+            isEligibleForFPCare() {
+              return true;
+            },
+            isEligibleForSuppBen() {
+              return true;
+            },
           }
         }
       }
     });
+
+    router = new VueRouter();
   });
   it('renders', () => {
     const wrapper = shallowMount(FormSelectionPage, {
       store,
+      router,
       localVue,
     });
     expect(wrapper.element).toBeDefined();
