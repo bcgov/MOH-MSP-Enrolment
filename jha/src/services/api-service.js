129817c2c941c2d3d5aaf2490ff9f4ba8aea38dd
Harry Maynard <harry.maynard@maximusbc.ca>
2022-01-19 17:28:03 -0500

    JHA: submission payload (WIP).

--- jha/src/services/api-service.js
+++ jha/src/services/api-service.js
@@ -1,18 +1,87 @@
 import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-import { formatISODate } from 'common-lib-vue';
+import {
+  formatISODate,
+  stripPhoneFormatting,
+  stripSpaces,
+} from 'common-lib-vue';
 
 const BASE_API_PATH = '/ahdc/api/';
-const SUBMIT_FORM_A_APPLICATION_URL = BASE_API_PATH + 'jhaIntegration/payPatientSubmission';
+const SUBMIT_APPLICATION_URL = BASE_API_PATH + 'jhaIntegration/payPatientSubmission';
 
 class ApiService {
   submitEnrolmentApplication(token, formState) {
     const jsonPayload = {
       applicationUuid: formState.applicationUuid,
-      requestUuid: uuidv4(),
-      submissionDate: formatISODate(formState.submissionDate) || '',
+      firstName: formState.ahFirstName || '',
+      secondName: formState.ahMiddleName || '',
+      lastName: formState.ahLastName || '',
+      sin: stripSpaces(formState.ahSIN) || '',
+      phn: '',
+      gender: formState.ahGender || '',
+      birthDate: formatISODate(formState.ahBirthdate) || null,
+      telephone: stripPhoneFormatting(formState.phone) || '',
+      addressLine1: formState.resAddressLine1 || '',
+      addressLine2: formState.resAddressLine2 || '',
+      addressLine3: formState.resAddressLine3 || '',
+      city: formState.resCity || '',
+      postalCode: formState.resPostalCode || '',
+      provinceOrState: formState.resProvince || '',
+      country: formState.resCountry || '',
+      authorizedByApplicant: 'Y', // TODO.
+      authorizedByApplicantDate: null, // TODO.
+      authorizedBySpouse: 'Y', // TODO.
+      spouse: null,
     };
-    return this._sendPostRequest(SUBMIT_FORM_A_APPLICATION_URL, token, jsonPayload);
+    if (formState.hasSpouse === 'Y') {
+      jsonPayload.spouse = {
+        firstName: formState.spouseFirstName || '',
+        secondName: formState.spouseMiddleName || '',
+        lastName: formState.spouseLastName || '',
+        gender: formState.spouseGender || '',
+        birthDate: formatISODate(formState.spouseBirthDate) || null,
+        telephone: stripPhoneFormatting(formState.phone) || '',
+        sin: stripSpaces(formState.spouseSIN) || '',
+        phn: stripSpaces(formState.spousePHN) || '',
+      };
+    }
+    if (formState.isApplyingForMSP) {
+      jsonPayload.medicalServicesPlan = {
+        uuid: formState.MSPUUID || '',
+        citizenshipType: formState.ahCitizenshipStatus || '',
+        attachmentUuids: formState.ahNameChangeSupportDocuments.map((image) => image.uuid),
+        residencyAttachmentUuids: formState.ahCitizenshipSupportDocuments.map((image) => image.uuid),
+        hasPreviousCoverage: formState.ahHasPreviousPHN || '',
+        prevPHN: formState.ahPreviousPHN || '',
+        hasLivedInBC: formState.ahHasLivedInBCSinceBirth || '',
+        prevHealthNumber: formState.ahPreviousHealthNumber || '',
+        recentBCMoveDate: formatISODate(formState.ahArrivalDateInBC) || null,
+        recentCanadaMoveDate: formatISODate(formState.ahArrivalDateInCanada) || null,
+        isPermanentMove: formState.ahIsMovedToBCPermanently || '',
+        prevProvinceOrCountry: formState.ahFromProvinceOrCountry || '',
+        beenOutsideBCMoreThan: formState.ahIsOutsideBCLast12Months || '',
+        departureDate: formatISODate(formState.ahOutsideBCLast12MonthsDepartureDate) || null,
+        returnDate: formatISODate(formState.ahOutsideBCLast12MonthsReturnDate) || null,
+        familyMemberReason: formState.ahOutsideBCLast12MonthsReason || '',
+        destination: formState.ahOutsideBCLast12MonthsLocation || '',
+        isFullTimeStudent: formState.ahIsStudent || '',
+        isInBCafterStudies: formState.ahWillStudentResideInBC || '',
+        armedDischargeDate: formatISODate(formState.ahArmedForcesDischargeDate) || null,
+        armedForceInstitutionName: '', // Unknown field that the middleware is collecting.
+      };
+      // Add mailing address.
+      if (!formState.isMailSame) {
+        jsonPayload.medicalServicesPlan.mailingAddress = {
+          addressLine1: formState.mailAddressLine1 || '',
+          addressLine2: formState.mailAddressLine2 || '',
+          addressLine3: formState.mailAddressLine3 || '',
+          city: formState.mailCity || '',
+          postalCode: stripSpaces(formState.mailPostalCode) || '',
+          provinceOrState: formState.mailProvince || '',
+          country: formState.mailCountry || '',
+        };
+      }
+    }
+    return this._sendPostRequest(SUBMIT_APPLICATION_URL, token, jsonPayload);
   }
 
   _sendPostRequest(url, token, jsonPayload) {
