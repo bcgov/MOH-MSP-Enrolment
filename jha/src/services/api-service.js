897a4bd7079c1936f4315b82040a7d7f65dfd914
Bryce Mcmath <bryce.mcmath@maximuscanada.ca>
2022-03-22 15:22:28 -0700

    Add special character replacement

--- jha/src/services/api-service.js
+++ jha/src/services/api-service.js
@@ -3,6 +3,7 @@ import {
   formatISODate,
   stripPhoneFormatting,
   stripSpaces,
+  replaceSpecialCharacters,
 } from 'common-lib-vue';
 import { ChildAgeTypes } from '@/constants/child-age-types';
 import { getCitizenshipType } from '@/constants/immigration-status-types';
@@ -38,7 +39,7 @@ class ApiService {
       city: formState.resCity || null,
       postalCode: stripSpaces(formState.resPostalCode) || null,
       provinceOrState: formState.resProvince || null,
-      country: formState.resCountry ? formState.resCountry.substring(0, 30) : '',
+      country: formState.resCountry ? replaceSpecialCharacters(formState.resCountry).substring(0, 30) : null,
       authorizedByApplicant: 'Y',
       authorizedByApplicantDate: formatISODate(dateToday),
       authorizedBySpouse: null, // Set below.
@@ -53,7 +54,7 @@ class ApiService {
       jsonPayload.city = formState.mailCity || null;
       jsonPayload.postalCode = stripSpaces(formState.mailPostalCode) || null;
       jsonPayload.provinceOrState = formState.mailProvince || null;
-      jsonPayload.country = formState.mailCountry || null;
+      jsonPayload.country = formState.mailCountry ? replaceSpecialCharacters(formState.mailCountry).substring(0, 30) : null;
     }
 
     if (formState.hasSpouse === 'Y') {
@@ -75,11 +76,11 @@ class ApiService {
       // formmatting for multiple values
       let fromProvinceOrCountry; 
       if (formState.ahFromProvinceOrCountry) {
-        fromProvinceOrCountry = formState.ahFromProvinceOrCountry.substring(0, 25);
+        fromProvinceOrCountry = replaceSpecialCharacters(formState.ahFromProvinceOrCountry).substring(0, 25);
       } else if (formState.ahMoveFromOrigin) {
-        fromProvinceOrCountry = formState.ahMoveFromOrigin.substring(0, 25);
+        fromProvinceOrCountry = replaceSpecialCharacters(formState.ahMoveFromOrigin).substring(0, 25);
       } else {
-        fromProvinceOrCountry = '';
+        fromProvinceOrCountry = null;
       }
 
       jsonPayload.medicalServicesPlan = {
@@ -120,7 +121,7 @@ class ApiService {
           recentBCMoveDate: formatISODate(formState.spouseRecentBCMoveDate) || null,
           recentCanadaMoveDate: formatISODate(formState.spouseCanadaArrivalDate) || null,
           isPermanentMove: formState.spouseMadePermanentMove || null,
-          prevProvinceOrCountry: formState.spouseMoveFromOrigin ? formState.spouseMoveFromOrigin.substring(0, 25) : null,
+          prevProvinceOrCountry: formState.spouseMoveFromOrigin ? replaceSpecialCharacters(formState.spouseMoveFromOrigin).substring(0, 25) : null,
           beenOutsideBCMoreThan: formState.spouseOutsideBCLast12Months || null,
           departureDate: formatISODate(formState.spouseOutsideBCLast12MonthsDepartureDate) || null,
           returnDate: formatISODate(formState.spouseOutsideBCLast12MonthsReturnDate) || null,
@@ -154,7 +155,7 @@ class ApiService {
               recentBCMoveDate: formatISODate(child.recentBCMoveDate) || null,
               recentCanadaMoveDate: formatISODate(child.canadaArrivalDate) || null,
               isPermanentMove: child.madePermanentMove || null,
-              prevProvinceOrCountry: child.moveFromOrigin ? child.moveFromOrigin.substring(0, 25) : null,
+              prevProvinceOrCountry: child.moveFromOrigin ? replaceSpecialCharacters(child.moveFromOrigin).substring(0, 25) : null,
               beenOutsideBCMoreThan: child.outsideBCLast12Months || '',
               departureDate: formatISODate(child.outsideBCLast12MonthsDepartureDate) || null,
               returnDate: formatISODate(child.outsideBCLast12MonthsReturnDate) || null,
@@ -186,7 +187,7 @@ class ApiService {
                 city: dependent.schoolCity || null,
                 postalCode: dependent.schoolPostalCode || null,
                 provinceOrState: dependent.schoolProvinceOrState || null,
-                country: dependent.schoolCountry ? dependent.schoolCountry.substring(0, 30) : null,
+                country: dependent.schoolCountry ? replaceSpecialCharacters(dependent.schoolCountry).substring(0, 30) : null,
               },
               citizenshipType: dependent.status || null,
               attachmentUuids: [
@@ -200,7 +201,7 @@ class ApiService {
               recentBCMoveDate: formatISODate(dependent.recentBCMoveDate) || null,
               recentCanadaMoveDate: formatISODate(dependent.canadaArrivalDate) || null,
               isPermanentMove: dependent.madePermanentMove || null,
-              prevProvinceOrCountry: dependent.moveFromOrigin ? dependent.moveFromOrigin.substring(0, 25) : null,
+              prevProvinceOrCountry: dependent.moveFromOrigin ? replaceSpecialCharacters(dependent.moveFromOrigin).substring(0, 25) : null,
               beenOutsideBCMoreThan: dependent.outsideBCLast12Months || null,
               departureDate: formatISODate(dependent.outsideBCLast12MonthsDepartureDate) || null,
               returnDate: formatISODate(dependent.outsideBCLast12MonthsReturnDate) || null,
@@ -223,7 +224,7 @@ class ApiService {
           city: formState.mailCity || null,
           postalCode: stripSpaces(formState.mailPostalCode) || null,
           provinceOrState: formState.mailProvince || null,
-          country: formState.mailCountry ? formState.mailCountry.substring(0, 30) : null,
+          country: formState.mailCountry ? replaceSpecialCharacters(formState.mailCountry).substring(0, 30) : null,
         };
       } else {
         jsonPayload.medicalServicesPlan.mailingAddress = {
@@ -233,7 +234,7 @@ class ApiService {
           city: formState.resCity || null,
           postalCode: stripSpaces(formState.resPostalCode) || null,
           provinceOrState: formState.resProvince || null,
-          country: formState.resCountry ? formState.resCountry.substring(0, 30) : null,
+          country: formState.resCountry ? replaceSpecialCharacters(formState.resCountry).substring(0, 30) : null,
         };
       }
 
