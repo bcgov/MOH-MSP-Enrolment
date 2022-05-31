d3412f1d5a35bbe18baa9496b65e9ac49b2aa26b
Jonathan Langlois <jonathan.langlois@maximuscanada.ca>
2022-05-31 11:45:18 -0700

    test for rendering fpc message

---
+++ jha/tests/unit/views/enrolment/SubmissionPage.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vue from 'vue';
+import SubmissionPage from '@/views/enrolment/SubmissionPage.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fpcAddressUpdateMiddlewareHTML } from "../../fixtures.js";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+localVue.component('font-awesome-icon', FontAwesomeIcon);
+
+describe('Enrolment - FormSelectionPage.vue', () => {
+  let state;
+  let store;
+
+  beforeEach(() => {
+    state = {
+      applicationUuid: null,
+      submissionAPIResponse: {
+        fpc: {
+          returnCode: '0',
+          message: fpcAddressUpdateMiddlewareHTML,
+        }
+      },
+    };    
+
+    store = new Vuex.Store({
+      modules: {
+        appModule: {
+          state: {
+            showMobileStepperDetails: false,
+          }
+        },
+        enrolmentModule: {
+          state,
+        }
+      }
+    });
+  });
+
+  it('Shows returned middleware messages for successful FPC only applications', () => {
+    const wrapper = shallowMount(SubmissionPage, {
+      store,
+      localVue,
+    });
+    const middlwareResponseContainer = wrapper.get('.middleware-response');
+  });
+});
