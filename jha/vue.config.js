f1199772ff33b730adcfc1d7647006ec7d974739
Jonathan Langlois <jonathan.langlois@maximuscanada.ca>
2022-06-09 10:10:51 -0700

    remove npm link setup

--- jha/vue.config.js
+++ jha/vue.config.js
@@ -11,9 +11,4 @@ module.exports = {
     }
   },
   lintOnSave: true,
-  // Below configuration is for use with npm link. Can be removed after common-lib-vue is working and published for vue-3
-  chainWebpack: (config) => {
-    config.resolve.symlinks(false)
-    config.resolve.alias.set( 'vue', path.resolve('./node_modules/vue'))
-  }
 };
