d994e973de89f1291c042d57fce9f7c58262d17e
Stephanie Harrison <stephanie.harrison@maximuscanada.ca>
2023-03-21 12:05:43 -0700

    add bc postal code validator tests to validators.spec.js

--- jha/tests/unit/helpers/validators.spec.js
+++ jha/tests/unit/helpers/validators.spec.js
@@ -1,4 +1,38 @@
-import { nameValidator } from "@/helpers/validators";
+import { bcPostalCodeValidator, nameValidator } from "@/helpers/validators";
+
+describe.only("bcPostalCodeValidator()", () => {
+  it("returns false when passed no input", async () => {
+    expect(bcPostalCodeValidator()).toBe(false);
+  });
+
+  it("returns true when passed a valid BC postal code", async () => {
+    expect(bcPostalCodeValidator("V8W1L4")).toBe(true);
+  });
+
+  it("returns true when passed a valid BC postal code with a space in it", async () => {
+    expect(bcPostalCodeValidator("V8W 1L4")).toBe(true);
+  });
+
+  it("returns false when passed a postal code that doesn't start with V", async () => {
+    expect(bcPostalCodeValidator("A8W1L4")).toBe(false);
+  });
+
+  it("returns false when passed a postal code that doesn't start with V and has a space in it", async () => {
+    expect(bcPostalCodeValidator("A8W 1L4")).toBe(false);
+  });
+
+  it("returns false when passed input containing non-alphanumeric characters", async () => {
+    expect(bcPostalCodeValidator("V8W1L@")).toBe(false);
+  });
+
+  it("returns false when passed input more than 8 characters long", async () => {
+    expect(bcPostalCodeValidator("V8W 1L4AAAAAA")).toBe(false);
+  });
+
+  it("returns false when passed input fewer than 6 characters long", async () => {
+    expect(bcPostalCodeValidator("V8W1L")).toBe(false);
+  });
+});
 
 describe("nameValidator()", () => {
   it("returns true when passed no input", async () => {
