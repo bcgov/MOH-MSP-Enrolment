f0d5f2b752b864b442bb3684769d17f946f3991d
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2023-07-26 14:57:49 -0700

    update list of files excluded from jest test coverage

--- jha/jest.config.js
+++ jha/jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
   collectCoverageFrom: [
     "src/**/*.{js,vue}",
     "!src/main.js", // No need to cover bootstrap file
+    "!src/locale/strings.en.js", // No need to cover strings
+    "!src/settings.js"
   ],
 }
