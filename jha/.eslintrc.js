2cfacd883f31743fc722bc1a8ce201760b755e01
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-07-17 15:52:29 -0700

    update eslint parser

--- jha/.eslintrc.js
+++ jha/.eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2022: true,
   },
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-    "requireConfigFile": false,
-  },  
+  ],  
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
