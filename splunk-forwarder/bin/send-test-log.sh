ff2ff966b66779eec540dcc08c7adc02e0748a1c
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-09-25 10:41:44 -0700

    update comments, authorization header in test log command

--- splunk-forwarder/bin/send-test-log.sh
+++ splunk-forwarder/bin/send-test-log.sh
@@ -1,3 +1,3 @@
-FORWARDER_PORT=8080 #needs to match the forwarder port used in run-local
+FORWARDER_PORT=8080 #needs to match the forwarder port used in start-local-forwarder.sh
 
-curl -XPOST -H "Authorization: Splunk XXX" -H "Content-Type: application/json" -d '{"body": "xyz", "logsource":"test curl request" }' localhost:${FORWARDER_PORT}/log
+curl -XPOST -H "Authorization: Api-Token XXX" -H "Content-Type: application/json" -d '{"body": "xyz", "logsource":"test curl request" }' localhost:${FORWARDER_PORT}/log
