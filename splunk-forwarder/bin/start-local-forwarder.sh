ea18cf360a006f87c2b3100fe37d1592617096f0
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-10-04 13:48:30 -0700

    update development tooling to work with test suite

--- splunk-forwarder/bin/start-local-forwarder.sh
+++ splunk-forwarder/bin/start-local-forwarder.sh
@@ -13,19 +13,22 @@ export SERVICE_USE_AUTH=false
 export SERVICE_AUTH_TOKEN=aaa #never copy live tokens into this file
 export SPLUNK_AUTH_TOKEN=aaa  #never copy live tokens into this file
 export CA_CERT=aaa            #never copy live certificates into this file
+export MONITOR_USERNAME=monitorusername #never copy live credentials into this file
+export MONITOR_PASSWORD=monitorpassword #never copy live credentials into this file
 export LOG_DIR_NAME=./logs
 export FILE_LOG_LEVEL=info
 export APPEND_POD_NAME_TO_FILE=false
 export SPLUNK_URL=http://localhost:${MOCK_LOGGER_PORT}/mock-logger #changing out this URL for the live logging endpoint won't work due to the firewall. Work of that nature will need to be done from within OpenShift
 export RETRY_COUNT=2
+export MAX_FILES=1
+export MAX_BYTE_SIZE_PER_FILE=78643200
 
-args="-XPOST localhost:${MOCK_LOGGER_PORT}/mock-logger"
-
-if curl -f $args; then
-    echo "Mock-logger is online, proceeding with Splunk Forwarder bootup"
+if [ $1 = "--test" ]; then
+    echo "Running in test mode (local forwarder only)"
+    node src/index.js server 
 else
-    echo "This script won't work as expected unless you have the mock-logger running. Run 'npm run start-mock-logger' in a separate terminal window first."
-    exit 0
+    echo "Running in development mode (mock-logger and forwarder)"
+    nodemon bin/mock-logger.js & nodemon src/index.js server
 fi
 
-nodemon src/index.js server
+
