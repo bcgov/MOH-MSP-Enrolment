c296629b9d06140f628ea32b0bb323960b27d99b
cyrilbravo <cbravo@gov.bc.ca>
2026-06-17 13:32:54 -0700

    fixed: remove tests for RetroPA

--- msp/src/app/app.component.spec.ts
+++ msp/src/app/app.component.spec.ts
@@ -46,12 +46,6 @@ describe('GeneralAppComponent', () => {
       expect(spy).toHaveBeenCalled();
     });
 
-    it('should clear storage if route is /assistance/home', () => {
-      const spy = spyOn(component, 'clearStorage').and.stub();
-      component.handleRefresh('/assistance/home');
-      expect(spy).toHaveBeenCalled();
-    });
-
     it('should do nothing if route is /deam/confirmation', () => {
       const spyRedirect = spyOn(component, 'hardRedirect').and.stub();
       const spyStorage = spyOn(component, 'clearStorage').and.stub();
@@ -60,23 +54,9 @@ describe('GeneralAppComponent', () => {
       expect(spyStorage).not.toHaveBeenCalled();
     });
 
-    it('should do nothing if route is /assistance/confirmation', () => {
-      const spyRedirect = spyOn(component, 'hardRedirect').and.stub();
-      const spyStorage = spyOn(component, 'clearStorage').and.stub();
-      component.handleRefresh('/assistance/confirmation');
-      expect(spyRedirect).not.toHaveBeenCalled();
-      expect(spyStorage).not.toHaveBeenCalled();
-    });
-
     it('should hard redirect if route is /deam/*, where * is anything other than home or confirmation', () => {
       const spy = spyOn(component, 'hardRedirect').and.stub();
       component.handleRefresh('/deam/personal-info');
-      expect(spy).toHaveBeenCalled();
-    });
-
-    it('should hard redirect if route is /assistance/*, where * is anything other than home or confirmation', () => {
-      const spy = spyOn(component, 'hardRedirect').and.stub();
-      component.handleRefresh('/assistance/personal-info');
       expect(spy).toHaveBeenCalled();
     });
   });
