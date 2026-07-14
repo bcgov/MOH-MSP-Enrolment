9ded2cd34011c4a0c52cd42aad0829df159bd780
cyrilbravo <cbravo@gov.bc.ca>
2026-07-14 16:18:22 -0700

    fixed: ran npm run build

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '85e8e90';
-export const buildTime = '7/14/2026 at 12:19:21 PM';
+export const gitCommit = 'b1593ce';
+export const buildTime = '7/14/2026 at 4:16:00 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.6.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 85e8e90 - 7/14/2026 at 12:19:21 PM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/85e8e90';
+export const message = 'Canonical Version: b1593ce - 7/14/2026 at 4:16:00 PM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/b1593ce';
 /** If true, other values should be present. */
 export const success = true;
