743e238f1a0ab52b5af1cf1fe20f51fd0fd7a0d1
cyrilbravo <cbravo@gov.bc.ca>
2026-07-29 14:04:46 -0700

    fixed: add missing fields in Review Page

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '0916ebd';
-export const buildTime = '7/21/2026 at 2:43:35 PM';
+export const gitCommit = '2ee0fc7';
+export const buildTime = '7/29/2026 at 1:50:50 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.6.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 0916ebd - 7/21/2026 at 2:43:35 PM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/0916ebd';
+export const message = 'Canonical Version: 2ee0fc7 - 7/29/2026 at 1:50:50 PM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/2ee0fc7';
 /** If true, other values should be present. */
 export const success = true;
