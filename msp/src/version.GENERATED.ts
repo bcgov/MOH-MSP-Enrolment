3b3edf44c7ea23ca85296bdf0d254a5c58249a69
Cyril Bravo <47836606+cyrilbravo@users.noreply.github.com>
2026-08-10 12:52:42 -0700

    fixed: add additional info for School Info and updated updateChildren data (#373)
    Co-authored-by: cyrilbravo <cbravo@gov.bc.ca>

--- msp/src/version.GENERATED.ts
+++ msp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '2ee0fc7';
-export const buildTime = '7/29/2026 at 1:50:50 PM';
+export const gitCommit = '0881461';
+export const buildTime = '8/7/2026 at 12:10:05 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '3.6.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 2ee0fc7 - 7/29/2026 at 1:50:50 PM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/2ee0fc7';
+export const message = 'Canonical Version: 0881461 - 8/7/2026 at 12:10:05 AM (v3.6.0) \nhttps://github.com/bcgov/MOH-MSP-Enrolment/commits/0881461';
 /** If true, other values should be present. */
 export const success = true;
